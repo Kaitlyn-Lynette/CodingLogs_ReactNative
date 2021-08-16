9e420b5325d779338c95d4359293a29f747392fe
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-08-16 14:25:39 -0400

    Added button styling

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default function App() {
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
+    setIsAddMode(false);
   };
 
   const removeGoalHandler = goalId => {
@@ -22,11 +23,15 @@ export default function App() {
     })
   }
 
+  const cancelGoalAdditionalHandler = () => {
+    setIsAddMode(false)
+  }
+
   return (
   
     <View style={styles.screen}>
       <Button title="Add New Goal" onPress={()=> setIsAddMode(true)}/>
-      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionalHandler}/>
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -8,6 +8,11 @@ const GoalInput = props => {
         setEnteredGoal(enteredText);
       };
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal(' ');
+    }
+
         return (
         <Modal visible={props.visible} animationType="slide">
         <View style={styles.inputContainer}>
@@ -17,7 +22,10 @@ const GoalInput = props => {
                 onChangeText={goalInputHandler}
                 value={enteredGoal}
             />
-            <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+            <View style={styles.buttonContainer}>
+            <Button title="CANCEL"  onPress={props.onCancel}color="red" />
+            <Button title="ADD" onPress={addGoalHandler} />
+            </View>
         </View>
         </Modal>
         )
@@ -28,8 +36,8 @@ const GoalInput = props => {
 
  const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
         },
     input: {
@@ -38,4 +46,9 @@ const GoalInput = props => {
         borderWidth: 1,
         padding: 10
         }, 
+    buttonContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        width: '60%'
+        }
 })
