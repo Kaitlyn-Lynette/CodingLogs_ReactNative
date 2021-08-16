2b2bd17554e4c2f4297c4accaec0cedd4ac129c8
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-08-16 13:31:25 -0400

    Added components

--- App.js
+++ App.js
@@ -1,48 +1,40 @@
 import React, {useState} from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, Text, View, Button, ScrollView, FlatList} from 'react-native';
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }
 
-  const addGoalHandler = () => {
-    console.log(enteredGoal)
-  }
+  const [courseGoals, setCourseGoals] = useState([]);
+
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
   return (
     <View style={styles.screen}>
-    <View style={styles.inputContainer}>
-      <TextInput placeholder="Enter log" 
-      style={styles.input} 
-      onChangeText={goalInputHandler}
-      value={enteredGoal}
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+         <GoalItem title={itemData.item.value} />
+        )}
       />
-      <Button title="ADD" 
-      onPress={addGoalHandler}/>
+     
     </View>
-    <View />
-  </View>
-);
-
-};
+  );
+}
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  }
+
 });
 
 
